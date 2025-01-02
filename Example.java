1f1f1c65e148fcd7e97079e6e4662e5989a4c328
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-01-02 14:36:58 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,20 @@
+public class Example {
+    public static void main(String[] args) {
+        int num1,num2,num3;
+        num1 = 50;
+        num2 = 60;
+        num3 = 90;
+
+        if(num1>num2){
+            System.out.println("num1 is greater");
+        }
+            else if(num2>num3){
+                System.out.println("num2 is greater");
+            }
+                else{
+                    System.out.println("num3 is greater");
+                }
+            }
+
+    }
+

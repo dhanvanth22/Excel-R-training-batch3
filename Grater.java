f456686813a4568feecfebbcb3bbb565f8e8638a
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-01-02 14:32:50 +0530

    Add files via upload

---
+++ Grater.java
@@ -0,0 +1,12 @@
+public class Grater {
+    public static void main(String[] args) {
+        int A =10;
+        int B =20;
+        if(A>B){
+            System.out.println("A is grater");
+        }
+        else{
+            System.out.println("B is grater");
+        }
+    }
+}

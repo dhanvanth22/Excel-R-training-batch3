a9f04b53cc9b509f1c9e149eb7febf5f5406f4e6
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-01-02 14:45:58 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -5,10 +5,10 @@ public class Example {
         num2 = 60;
         num3 = 90;
 
-        if(num1>num2){
+        if(num1>num2) && (num1>num3){
             System.out.println("num1 is greater");
         }
-            else if(num2>num3){
+            else if(num2>num1) && (num2>num3){
                 System.out.println("num2 is greater");
             }
                 else{

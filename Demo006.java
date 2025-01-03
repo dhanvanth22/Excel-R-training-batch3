cb1b444c37bc67c256ef3cbc62bbb73490b95a5f
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-01-03 10:44:59 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        String name ="dhanvanth123";
+        String pwd = "dhanvanth_123";
+
+        while((name == "dhanvanth123") && (pwd == "dhanvanth_123")){
+            System.out.println("Hello dhanvanth , welcome to malla reddy university");
+            break;
+        }
+    }
+}

d8841600cbedfbbfb9aad25acb40f98de014a66f
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-01-04 09:36:13 +0530

    Add files via upload

---
+++ Demo008.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Demo008{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username;
+        String password;
+        do {
+            System.out.print("Enter username: ");
+            username = scanner.nextLine();
+            System.out.print("Enter password: ");
+            password = scanner.nextLine();
+            if (username.equals("Mahesh") && password.equals("Mahesh@123")) {
+                System.out.println(username + "Welcome to Mallareddy University");
+                break;
+            } else {
+                System.out.println("Incorrect username or password. Please try again.\n");
+            }
+        } while (true);
+
+        scanner.close();
+    }
+}

45e909628289a7eff42c68064f7af4f7a7a37042
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-02-01 12:03:18 +0530

    Update Demo002.java

--- Demo002.java
+++ Demo002.java
@@ -1,20 +1,11 @@
-public class Demo002 {
-    public static void main(String[] args) {
-        int num1,num2,num3;
-        num1 = 50;
-        num2 = 60;
-        num3 = 90;
-
-        if(num1>num2) && (num1>num3){
-            System.out.println("num1 is greater");
+class Demo002{
+    public static void main(String args[] ){
+        int a = 50;
+        int b = 45;
+        if (a > b){
+            System.out.println(" Greater number is A:"+a);
+        }else{
+            System.out.println(" Greater number is B:"+b);
         }
-            else if(num2>num1) && (num2>num3){
-                System.out.println("num2 is greater");
-            }
-                else{
-                    System.out.println("num3 is greater");
-                }
-            }
-
     }
-
+}

bbf55dba4953d40cd68b2d867b1d6e8fecd4563a
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-02-01 12:04:24 +0530

    Update Demo004.java

--- Demo004.java
+++ Demo004.java
@@ -1,6 +1,6 @@
 class Demo004{
     public static void main(String[] args) {
-        for(int i = 1; i<=100;i++){
+        for(int i = 0; i<=100;i++){
             System.out.println(i);
         }
     }

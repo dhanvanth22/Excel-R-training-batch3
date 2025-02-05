47f269fbc54602157e79f9a4a9babb32ba254a77
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-02-05 12:18:48 +0530

    Add files via upload

---
+++ Demo065.java
@@ -0,0 +1,12 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        // Example array
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Print array elements
+        System.out.print("Array elements: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}

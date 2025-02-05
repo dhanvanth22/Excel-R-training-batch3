3182dc430757d502b88c4a148db99189d9dcab00
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-02-05 12:20:43 +0530

    Add files via upload

---
+++ Demo066.java
@@ -0,0 +1,12 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        // Example array
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Print index values of the array
+        System.out.println("Index values of the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + ": " + numbers[i]);
+        }
+    }
+}

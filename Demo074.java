be6faff2e86b87400b73d8e24c45743ecf966563
dhanvanth.18 <156776958+dhanvanth22@users.noreply.github.com>
2025-02-12 12:31:56 +0530

    Add files via upload

---
+++ Demo074.java
@@ -0,0 +1,39 @@
+// Demo074.java
+public class Demo074 {
+    
+    // Method to sort an array using Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        // Sorting the array
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
